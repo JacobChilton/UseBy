4379c23e58ab9307eec5d77c7486a50ffa5979b0
louis2374 <115995518+louis2374@users.noreply.github.com>
2025-03-03 22:26:23 +0000

    Add .env parse

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,7 +1,15 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import path from "path"
+
+// Putting this here, incase the file is called from other than index
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 
 // Replace the uri string with your connection string.
-const uri = "mongodb://mongo:password@localhost:27017";
+const uri = `mongodb://${username}:${password}@localhost:27017`;
 const client = new MongoClient(uri);
 
 
---
+++ server/src/index.ts
@@ -0,0 +1,4 @@
+import dotenv from "dotenv";
+
+// Putting this here, incase the file is called from other than index
+dotenv.config({ path: ".env" });
