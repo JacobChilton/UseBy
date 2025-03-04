1a7825167547c04e98a1798da983b9c8069b8aaf
louis2374 <115995518+louis2374@users.noreply.github.com>
2025-03-04 23:47:58 +0000

    Rename response to std_response

--- server/src/util/standard_response.ts
+++ server/src/util/standard_response.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 import { HTTP } from "./http";
 
-export const response = (p_res: Response, p_status: HTTP, p_json: Record<string, any>) =>
+export const std_response = (p_res: Response, p_status: HTTP, p_json: Record<string, any>) =>
 {
     p_res.status(p_status).json(p_json);
 }
