db45bb460df614bf343714495296d8ee610df726
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2025-03-04 22:50:16 +0000

    Update password_hash

--- server/src/auth/password_hash.ts
+++ server/src/auth/password_hash.ts
@@ -1,12 +1,21 @@
 import crypto from "crypto";
 
-export const password_hash = (p_password:string) => {
+export const password_hash = (p_password:string, p_salt?:string) => {
 
-    const salt = crypto.randomBytes(16);
+    let salt;
+
+    if (p_salt) { // If salt was passed in
+        
+        salt = p_salt; // Use the passed in salt
+    }
+    else { // If salt was not passed in
+
+        salt = crypto.randomBytes(16); // Generate salt
+    }
 
     return new Promise((resolve, reject) =>
     {
-        crypto.scrypt(p_password, salt, 32, (err, derivedKey) =>
+        crypto.scrypt(p_password, salt, 32, (err, derivedKey) => // Hashes password and adds salt
             {
                 if (err) reject(err);
 
