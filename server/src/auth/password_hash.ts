1ef62b2e0dcb111bbef633192d2379bd2971620b
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2025-03-04 22:38:27 +0000

    Create password_hash()

---
+++ server/src/auth/password_hash.ts
@@ -0,0 +1,20 @@
+import crypto from "crypto";
+
+export const password_hash = (p_password:string) => {
+
+    const salt = crypto.randomBytes(16);
+
+    return new Promise((resolve, reject) =>
+    {
+        crypto.scrypt(p_password, salt, 32, (err, derivedKey) =>
+            {
+                if (err) reject(err);
+
+                else resolve(derivedKey.toString("hex") + ":" + salt);
+            });
+    });
+}
+
+console.log(password_hash("password"));
+
+password_hash("password").then(console.log);
