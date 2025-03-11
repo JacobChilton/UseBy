6457af9a86d64bf2f680ba8fde2bbb089f75e1b0
louis2374 <115995518+louis2374@users.noreply.github.com>
2025-03-11 18:28:12 +0000

    Fix typeguard error on null

--- server/src/types/database_typeguards.ts
+++ server/src/types/database_typeguards.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from "mongodb";
-import { House, Product, User } from "./database";
+import { Availability, House, Product, User } from "./database";
 
 // These will be quite explicit, as i do not trust
 // mongo
@@ -8,14 +8,16 @@ import { House, Product, User } from "./database";
 
 export const tg_is_user = (p_obj: any): p_obj is User =>
 {
-    return p_obj._id instanceof ObjectId &&
+    return p_obj &&
+        p_obj._id instanceof ObjectId &&
         typeof p_obj.email === "string" &&
         typeof p_obj.password === "string";
 }
 
 export const tg_is_product = (p_obj: any): p_obj is Product =>
 {
-    return p_obj._id instanceof ObjectId &&
+    return p_obj &&
+        p_obj._id instanceof ObjectId &&
         typeof p_obj.upc === "string" &&
         p_obj.owner_id instanceof ObjectId &&
         p_obj.house_id instanceof ObjectId &&
@@ -26,9 +28,15 @@ export const tg_is_product = (p_obj: any): p_obj is Product =>
 
 export const tg_is_house = (p_obj: any): p_obj is House =>
 {
-    return p_obj._id instanceof ObjectId &&
+    return p_obj &&
+        p_obj._id instanceof ObjectId &&
         typeof p_obj.name === "string" &&
         p_obj.owner_id instanceof Object &&
         p_obj.members instanceof Array &&
         (p_obj.members as Array<unknown>).reduce<boolean>((prev, curr) => prev && curr instanceof ObjectId, true)
+}
+
+export const tg_is_availability = (p_obj: any): p_obj is Availability =>
+{
+    return Object.keys(Availability).includes(p_obj)
 }
