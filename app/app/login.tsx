601eb6400664e39a59bec93ae3833dd5e4807724
louis2374 <115995518+louis2374@users.noreply.github.com>
2025-03-24 16:46:32 +0000

    Idk

--- app/app/login.tsx
+++ app/app/login.tsx
@@ -6,96 +6,95 @@ import { TextInput, Button, DefaultTheme, PaperProvider, Text } from 'react-nati
 import { Link } from 'expo-router';
 
 const customTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    text: 'black',
-  },
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        text: 'black',
+    },
 };
 
-const Login: React.FC = () => {
-  const [email, set_email] = useState("");
-  const [password, set_password] = useState("");
-  const [error, set_error] = useState("");
+const Login: React.FC = () =>
+{
+    const [email, set_email] = useState("");
+    const [password, set_password] = useState("");
+    const [error, set_error] = useState("");
 
-  // Get logged_in and login from the context
-  const { logged_in, login } = useAPI();
+    // Get logged_in and login from the context
+    const { logged_in, login } = useAPI();
 
-  // This would go in root layout, if the user is logged in, it should just continue as normal
-  // If not logged in, it should route them to the login page
-  if (logged_in) return <Text>YOU ARE LOGGED IN</Text>;
+    // Basic login form, calling login(user,pass) sends a request to the server.
+    // If login is successful, the context will have a state change, causing all children to reload
+    // If it is not, e.message is the error message (something like invalid creds or failed to login)
+    return (
+        <PaperProvider theme={customTheme}>
+            <View className="flex-1 justify-center items-around p-8">
+                <Text>{error}</Text>
 
-  // Basic login form, calling login(user,pass) sends a request to the server.
-  // If login is successful, the context will have a state change, causing all children to reload
-  // If it is not, e.message is the error message (something like invalid creds or failed to login)
-  return (
-    <PaperProvider theme={customTheme}>
-      <View className="flex-1 justify-center items-around p-8">
-        <Text>{error}</Text>
+                <Text
+                    variant="displayLarge"
+                    style={{ fontWeight: 'bold' }}
+                >
+                    LOGIN
+                </Text>
 
-        <Text 
-          variant="displayLarge"
-          style={{ fontWeight: 'bold' }}
-        >
-          LOGIN
-        </Text>
+                <Text className="mb-12 mt-1 text-xl"> Please sign in to continue </Text>
 
-        <Text className="mb-12 mt-1 text-xl"> Please sign in to continue </Text>
+                {/* Input form */}
+                <TextInput
+                    onChangeText={set_email}
+                    placeholder='Enter your Email'
+                    left={<TextInput.Icon icon="email" />}
+                    mode="outlined"
+                    style={{ backgroundColor: 'transparent', width: '100%' }}
+                />
+                <TextInput
+                    onChangeText={set_password}
+                    placeholder='Enter your password'
+                    left={<TextInput.Icon icon="lock" />}
+                    mode="outlined"
+                    style={{ backgroundColor: 'transparent', width: '100%', marginTop: 20 }}
+                />
+                <Button
+                    icon="arrow-right"
+                    mode="contained"
+                    contentStyle={{ flexDirection: 'row-reverse' }}
+                    labelStyle={{ fontSize: 16 }}
+                    style={{
+                        width: 150,
+                        alignSelf: 'flex-end',
+                        marginTop: 20,
+                    }}
+                    onPress={() =>
+                    {
+                        login(email, password)
+                            .catch((e: APIError) =>
+                            {
+                                set_error(e.message);
+                            });
+                    }}
+                >
+                    Login
+                </Button>
 
-        {/* Input form */}
-        <TextInput 
-          onChangeText={set_email} 
-          placeholder='Enter your Email' 
-          left={<TextInput.Icon icon="email" />}
-          mode="outlined"
-          style={{ backgroundColor: 'transparent', width: '100%' }} 
-        />
-        <TextInput 
-          onChangeText={set_password} 
-          placeholder='Enter your password' 
-          left={<TextInput.Icon icon="lock" />}
-          mode="outlined"
-          style={{ backgroundColor: 'transparent', width: '100%', marginTop: 20 }} 
-        />
-        <Button 
-          icon="arrow-right" 
-          mode="contained" 
-          contentStyle={{ flexDirection: 'row-reverse' }}
-          labelStyle={{ fontSize: 16 }} 
-          style={{
-            width: 150, 
-            alignSelf: 'flex-end',
-            marginTop: 20,
-          }}
-          onPress={() => {
-            login(email, password)
-              .catch((e: APIError) => {
-                set_error(e.message);
-              });
-          }}
-        > 
-          Login 
-        </Button> 
-
-        <View style={{ flexDirection: 'row', marginTop: 20, alignSelf: 'flex-end' }}>
-          <Text style={{ fontSize: 16 }}>
-            Don't have an account?{" "}
-          </Text>
-          <Link href="/signup" asChild>
-            <Text
-              style={{
-                fontSize: 16,
-                fontWeight: 'bold',
-                color: DefaultTheme.colors.primary, 
-              }}
-            >
-              Sign Up
-            </Text>
-          </Link>
-        </View>
-      </View>
-    </PaperProvider>
-  );
+                <View style={{ flexDirection: 'row', marginTop: 20, alignSelf: 'flex-end' }}>
+                    <Text style={{ fontSize: 16 }}>
+                        Don't have an account?{" "}
+                    </Text>
+                    <Link href="/signup" asChild>
+                        <Text
+                            style={{
+                                fontSize: 16,
+                                fontWeight: 'bold',
+                                color: DefaultTheme.colors.primary,
+                            }}
+                        >
+                            Sign Up
+                        </Text>
+                    </Link>
+                </View>
+            </View>
+        </PaperProvider>
+    );
 };
 
 export default Login;
