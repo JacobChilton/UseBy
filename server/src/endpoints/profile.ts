f08a39d80deadaff9ef63c7940faf3a9949e5e72
louis2374 <115995518+louis2374@users.noreply.github.com>
2025-03-30 22:43:23 +0100

    Update profile endpoint to return picture id

--- server/src/endpoints/profile.ts
+++ server/src/endpoints/profile.ts
@@ -7,5 +7,5 @@ import { HTTP } from "../util/http";
 export const ep_profile_get = auth(async (req: Request, res: Response, user: User) =>
 {
     // Just send user email, and ect
-    std_response(res, HTTP.OK, { email: user.email, name: user.name, _id: user._id });
+    std_response(res, HTTP.OK, { email: user.email, name: user.name, _id: user._id, picture: user.picture });
 });
