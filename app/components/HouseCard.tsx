33559f89231a00670a23f525e16ff72598b2f225
louis2374 <115995518+louis2374@users.noreply.github.com>
2025-05-13 22:12:52 +0100

    Reapply "Fix houses refresh"
    This reverts commit 36ca4dfb2013883cc49d0e859d9dc7b4cd96e64a.

--- app/components/HouseCard.tsx
+++ app/components/HouseCard.tsx
@@ -50,17 +50,20 @@ const HouseCard: React.FC<Props> = ({ house, user, removed, invite }) =>
         {
             console.log("success");
             if (removed) removed();
+            api.set_ugly_duckling(Date.now())
         })
         .catch((e) =>
         {
             console.error(e);
         })
+        
 
     const delete_house = () => api.house_delete(house._id)
         .then(() =>
         {
             console.log("success");
             if (removed) removed();
+            api.set_ugly_duckling(Date.now())
         })
         .catch((e) =>
         {
